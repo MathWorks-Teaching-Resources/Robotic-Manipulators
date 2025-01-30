0d8c18e37d250f13b399e09bfba6f6b55cf44dfd
callie <callie@mathworks.com>
2025-01-30 14:16:54 -0600

    Full suite of solutions added with testing solutions.

---
+++ SoftwareTests/PreFiles/PreCoordinateSystemTransformations.m
@@ -0,0 +1,21 @@
+%  Pre-run script for CoordinateSystemTransformations.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "Fail on figures because empty without interaction";
+% ---- Pre-run commands -----
+ 
+fig1 = figure;
+fig2 = figure;
+ax1 = axes;
+
+fig4 = figure;
+fig5 = figure;
+ax2 = axes;
+
+fig6 = figure;
+FigFT = figure;
+FigINV = figure;
+
+f1 = figure;
+f2 = figure;
+ax3 = axes;
+ax4 = axes;
---
+++ SoftwareTests/PreFiles/PreDHParameters.m
@@ -0,0 +1,8 @@
+%  Pre-run script for DHParameters.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "Fail on figures because empty without interaction";
+% ---- Pre-run commands -----
+ 
+FigDH = figure;
+g1 = figure;
+ax5 = axes;
