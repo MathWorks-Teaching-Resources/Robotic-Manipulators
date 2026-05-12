902eaae3bf87589892e5468424e9aaba2c304c45
Emma Smith Zbarsky <esmithzb@mathworks.com>
2026-05-12 12:36:04 -0400

    Mock open_system in R2025b test

--- SoftwareTests/PreFiles/PreForwardKinematics.m
+++ SoftwareTests/PreFiles/PreForwardKinematics.m
@@ -3,5 +3,24 @@
 KnownIssuesID = "MATLAB:TooManyInputs";
 % ---- Pre-run commands -----
 
+<<<<<<< Updated upstream
 open_system = @(x) disp("open simulink for test");
 sim = @(y,z,p) disp("run simulink for test");
+=======
+releaseInfo = matlabRelease;
+
+if releaseInfo.Release == "R2025b"
+    open_system = @(str)MyOpen(str);
+    sim = @(str)MySim(str);
+end
+
+function MyOpen(str)
+assert(exist(str,"file"))
+disp("Opening the system " + str)
+end
+
+function MySim(str)
+disp("Simulating the system " + str);
+% Add simulation logic here
+end
+>>>>>>> Stashed changes
--- SoftwareTests/PreFiles/PreForwardKinematicsSoln.m
+++ SoftwareTests/PreFiles/PreForwardKinematicsSoln.m
@@ -2,7 +2,27 @@
 % ---- Known Issues     -----
 KnownIssuesID = "MATLAB:TooManyInputs";
 % ---- Pre-run commands -----
+<<<<<<< Updated upstream
 
 
 open_system = @(x) disp("open simulink for test");
 sim = @(y,z,p) disp("run simulink for test");
+=======
+ 
+releaseInfo = matlabRelease;
+
+if releaseInfo.Release == "R2025b"
+    open_system = @(str)MyOpen(str);
+    sim = @(str)MySim(str);
+end
+
+function MyOpen(str)
+assert(exist(str,"file"))
+disp("Opening the system " + str)
+end
+
+function MySim(str)
+disp("Simulating the system " + str);
+% Add simulation logic here
+end
+>>>>>>> Stashed changes
